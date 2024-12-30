f74af617800212f46c42df2fca1a11302876f0e8
Ines Fajardo <finesjanellys1996@hotmail.com>
2024-12-30 00:44:55 -0500

    cambio de ruta

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
-import {RouteLanding} from "./Routes/RouteLanding.jsx";
+import {RouteLanding} from "./routes/RouteLanding.jsx";
 import { AppProvider } from './context/AppProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
